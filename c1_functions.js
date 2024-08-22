71b85ee72944c6e0dacf453ed762fbff6ae8d626
Yash <tripathiyash143@gmail.com>
2024-08-22 11:11:56 +0530

    Different types of Arrow function

--- c1_functions.js
+++ c1_functions.js
@@ -3,3 +3,13 @@ sum=(a,b)=>a+b;
 console.log("Sum is:",sum(3,5));
 multiply=(a,b)=>a*b;
 console.log("Product is:",multiply(3,5));
+
+//optional paranthesis or single parameter
+var num=x=>{
+    console.log(x);
+
+}
+num(140);
+
+//optional braces for single statement
+
