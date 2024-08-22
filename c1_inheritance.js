03e1b6335478fb02d000a08d4fa747f83b2373bc
Yash <tripathiyash143@gmail.com>
2024-08-22 10:41:34 +0530

    Inheritance

---
+++ c1_inheritance.js
@@ -0,0 +1,19 @@
+class Car{
+    constructor(name){
+        this.brand=name;
+    }
+    present(){
+        return "I have a "+this.brand;
+    }
+}
+class Model extends Car{
+    constructor(name,mod){
+        super(name);
+        this.model =mod;
+    }
+    show(){
+        console.log(this.present()+',it is a '+this.model)
+    }
+}
+const myCar = new Model("Ford","Mustang");
+myCar.show();
