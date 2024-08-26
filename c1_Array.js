3284f3f349126e4f17b770355878fe9eb6b62b1f
Yash <tripathiyash143@gmail.com>
2024-08-26 20:00:11 +0530

    different operations on array

--- c1_Array.js
+++ c1_Array.js
@@ -24,7 +24,7 @@ console.log(name2.length)
 // console.log(num2.copyWithin(2,4));
 // console.log(num2.copyWithin(2));
 
-//Array.find -prints irst value following the given condition 
+//Array.find -prints first value following the given condition 
 const numbers=[4,9,16,25,29];
 let first=numbers.find(myFunction);
 function myFunction(value,index,array){
@@ -44,3 +44,42 @@ for(i of show){
 
 var index=[12,5,8,130].indexOf(130);
 console.log("index is:",index);
+
+//map function
+var number=[1,4,9];
+var roots=number.map(Math.sqrt);
+var mult=number.map((i)=>
+{
+    return i*2;
+});
+console.log(mult);
+console.log("Square roots are: ",roots)
+
+//spread operator
+var a=[1,2,3];
+var b=[4,5,6];
+var c=[7,8,9];
+let d=[...a, ...b, ...c];
+console.log(d);
+
+//Array.values
+var color1=["Red","Yellow","Blue","Black"];
+var s=color1.values();
+console.log(...s);
+
+//array.fill
+var s2=color1.fill("purple",2,4);
+console.log(...s2);
+
+//array destructuring
+var[c1,c2,c3]=color1;
+console.log(c1);
+console.log(c2);
+console.log(c3);
+
+//array destructuring and rest operator
+
+let [a1,b2,...args]=colors;
+console.log(a1);
+console.log(b2);
+console.log(args);
