986f04cd063744b826b3e41689c00dd795be407a
Yash <tripathiyash143@gmail.com>
2024-09-02 14:14:02 +0530

    jsx codes basics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  //Two tags cannot  be used without div
+  var name='across india';
+  return(
+    // <div>
+    <>
+    <p>Lovely professional University {name} {1000+1000}</p>
+    <h1> Best University</h1>
+    </>
+    //</></div>
   );
 }
 
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,51 @@
+//using jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+// ReactDOM.render(<h1>Lovely Professional University</h1>,document.getElementById("root"));
+
+
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+// using javascript
+// var h1=document.createElement("h1");
+// h1.innerHTML="Lovely Professional University";
+// document.getElementById('root').appendChild(h1);
+
+
+// import {Add,sub,Mul,Div} from './calculator';
+// import HelloWorld from './helloworld';
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App/>
+//     <HelloWorld/>
+//   </React.StrictMode>
+// );
+
+//jsx Attribute content editable property.
+const name="LPU";
+const img1="https://dummyimage.com/640x480"
+ReactDOM.render(
+  <>
+  <h1 contentEditable="true"> {name}</h1>
+  <img src={img1}/>
+  </>,
+  document.getElementById('root')
+)
 reportWebVitals();
