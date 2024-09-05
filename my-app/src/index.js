9a41737ac59982a3e98bce06b291369e02a1e041
Yash <tripathiyash143@gmail.com>
2024-09-05 10:18:42 +0530

    new components update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,13 +1,15 @@
 //using jsx
+// import React, { Component } from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Navigation from './navigation'
 // ReactDOM.render(<h1>Lovely Professional University</h1>,document.getElementById("root"));
 
 
 // import ReactDOM from 'react-dom/client';
-// import './index.css';
+import './index.css';
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -39,13 +41,58 @@ import reportWebVitals from './reportWebVitals';
 // );
 
 //jsx Attribute content editable property.
-const name="LPU";
-const img1="https://dummyimage.com/640x480"
-ReactDOM.render(
-  <>
-  <h1 contentEditable="true"> {name}</h1>
-  <img src={img1}/>
-  </>,
-  document.getElementById('root')
+// const name="LPU";
+// const img1="https://dummyimage.com/640x480"
+// ReactDOM.render(
+//   <>
+//   <h1 contentEditable="true"> {name}</h1>
+//   <img src={img1}/>
+//   </>,
+//   document.getElementById('root')
+// )
+// reportWebVitals();
+
+
+// Components in componenets
+
+// function Car(){
+//   return <h2>This is a car!</h2>;
+// }
+// function Garage(){
+//   return(
+//     <>
+//     <h1>
+//       The car is in Garage</h1>
+//       <Car/>
+//       </>
+//   );
+// }
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Garage/>);
+
+//Compnents in Props
+// function Car(props){
+//   return <h2>I am a {props.color} car!</h2>;
+// }
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Car color="Black"/>);
+
+
+//Components receiving props as argument.
+
+// function Car(props){
+//   return <h2>I am a {props.brand} car!</h2>;
+// }
+// const myElement=<Car brand="Ford"/>;
+// const root=ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myElement);
+
+
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<React.StrictMode>
+  <Navigation title="LPU" type="Contact us"/>
+</React.StrictMode>
 )
-reportWebVitals();
+
+
